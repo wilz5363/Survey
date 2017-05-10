e00f26b9c0ef5b5f6d35563934ee7599bf78b0c5
Wilson Chan <Wilson Chan>
2017-05-10 19:41:18 +0800

    Added user register api for android

---
+++ api/userRegister.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chanw
+ * Date: 5/10/2017
+ * Time: 7:26 PM
+ */
+$email = $_POST['Email'];
+$password = $_POST['Password'];
+
+require_once "../include/DbConnect.php";
+$db = new DbConnect();
+$conn = $db->connect();
+
+$sqlInsertUser = "insert into users(Email, Password) VALUES ('".$email."', '".$password."')";
+
+if(mysqli_query($conn. $sqlInsertUser)){
+    return "true";
+}else{
+    return "false";
+}
